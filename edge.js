525b4442077fe0aded0c729edabf3c8dbf098e70
q-lilianguo <123545915+q-lilianguo@users.noreply.github.com>
2023-03-16 16:50:17 +0800

    Move edge and node related methods to their own classes (#26)

--- edge.js
+++ edge.js
@@ -1,7 +1,7 @@
 class GraphEdge {
     constructor(source, target, attributes, label) {
-        const document = source.nodeType === "document" ? source : target;
-        const entity = source.nodeType === "entity" ? source : target;
+        const document = source.nodeType === GraphNode.nodeTypes.DOC ? source : target;
+        const entity = source.nodeType === GraphNode.nodeTypes.ENT ? source : target;
 
         this.id = String(source.id) + "+" + String(target.id);
         this.source = source;
@@ -9,5 +9,59 @@ class GraphEdge {
         this.type = document.type + '-' + entity.type;
         this.label = (label != "" && typeof label !== 'undefined') ? label : this.type + this.id;
         this.attributes = attributes;
+    }
+
+    getPathStartAndEndPoints() {
+        const sourceX = this.source.nodeType === GraphNode.nodeTypes.DOC ? this.source.x + GraphNode.consts.RECT_WIDTH/2 : this.source.x;
+        const sourceY = this.source.nodeType === GraphNode.nodeTypes.DOC ? this.source.y + GraphNode.consts.RECT_HEIGHT/2 : this.source.y;
+        const targetX = this.target.nodeType === GraphNode.nodeTypes.DOC ? this.target.x + GraphNode.consts.RECT_WIDTH/2 : this.target.x;
+        const targetY = this.target.nodeType === GraphNode.nodeTypes.DOC ? this.target.y + GraphNode.consts.RECT_HEIGHT/2 : this.target.y;
+        return "M" + sourceX + "," + sourceY + "L" + targetX + "," + targetY;
+    }
+
+    static makeEdge(edges) {
+        edges.append("path")
+            .attr("id", e => e.id)
+            .classed("line", true)
+            .attr("d", e => {
+                return e.getPathStartAndEndPoints();
+            });
+
+        edges.append("text")
+            .attr("class", "edge-label")
+            .attr("dy", - 15)
+            .append("textPath")
+            .attr("xlink:href", e => "#" + e.id)
+            .attr("text-anchor", "middle")
+            .attr("startOffset", "50%")
+            .text(e => e.label);
+    }
+
+    editEdgeLabel(paths, plot) {
+        const selection = paths.selectAll('g').filter(dval => {
+            return dval.id === this.id;
+        });
+        // hide current label
+        const text = selection.selectAll("text").classed("hidden", true);
+        // add intermediate editable paragraph
+        const d3txt = plot.selectAll("foreignObject")
+            .data([this])
+            .enter()
+            .append("foreignObject")
+            // TODO: rotate via transform: rotate(20deg);
+            .attr("x", this.target.x - (this.target.x - this.source.x) / 2)
+            .attr("y", this.target.y - (this.target.y - this.source.y) / 2)
+            .attr("height", 100)
+            .attr("width", 100)
+            .append("xhtml:div")
+            //.style("transform", "rotate(20deg)")
+            .attr("id", "editable-p")
+            .attr("contentEditable", "true")
+            .style("text-align", "center")
+            .text(this.label)
+            .on("mousedown", (event, d) => {
+                event.stopPropagation();
+            });
+        d3txt.node().focus();
     }
 }
--- node.js
+++ node.js
@@ -1,11 +1,89 @@
 class GraphNode {
+    static consts = {
+        NODE_RADIUS: 50,
+        RECT_WIDTH: 100,
+        RECT_HEIGHT: 100,
+    }
+
+    static nodeTypes = {
+        ENT: "entity",
+        DOC: "document"
+    }
+
     constructor(id, nodeType, type, attributes, pos, label) {
         this.id = id;
         this.nodeType = nodeType;
         this.type = type;
         this.label = (label != "" && typeof label !== 'undefined') ? label : type + "-" + id;
-        this.x = pos[0];
-        this.y = pos[1];
+        this.x = nodeType === GraphNode.nodeTypes.DOC ? pos[0]-GraphNode.consts.RECT_WIDTH/2 : pos[0];
+        this.y = nodeType === GraphNode.nodeTypes.DOC ? pos[1]-GraphNode.consts.RECT_HEIGHT/2 : pos[1];
         this.attributes = attributes;
+    }
+
+    static makeEntity(nodes) {
+        nodes.append("circle")
+            .attr("r", String(this.consts.NODE_RADIUS));
+
+        nodes.append("text")
+            .text(d => { return d.label; });
+    }
+
+    static makeDocument(nodes) {
+        nodes.append("rect")
+            .attr("width", this.consts.RECT_WIDTH)
+            .attr("height", this.consts.RECT_HEIGHT);
+
+        nodes.append("text")
+            .attr("dx", this.consts.RECT_WIDTH/2)
+            .attr("dy", this.consts.RECT_HEIGHT/2)
+            .text(d => { return d.label; });
+    }
+
+    editNodeLabel(circles, plot) {
+        const selection = circles.selectAll('g').filter(function (dval) {
+            return dval.id === this.id;
+        });
+        // hide current label
+        const text = selection.selectAll("text").classed("hidden", true);
+        // add intermediate editable paragraph
+        if (this.nodeType===GraphNode.nodeTypes.ENT) {
+            const d3txt = plot.selectAll("foreignObject")
+                .data([this])
+                .enter()
+                .append("foreignObject")
+                .attr("x", this.x - GraphNode.consts.NODE_RADIUS)
+                .attr("y", this.y - GraphNode.consts.NODE_RADIUS/2)
+                .attr("height", 2 * GraphNode.consts.NODE_RADIUS)
+                .attr("width", 2 * GraphNode.consts.NODE_RADIUS)
+                .append("xhtml:div")
+                .attr("id", "editable-p")
+                .attr("contentEditable", "true")
+                .style("text-align", "center")
+                .text(this.label)
+                .on("mousedown", (event, d) => {
+                    event.stopPropagation();
+                });
+            d3txt.node().focus();
+        }
+
+         else if (this.nodeType===GraphNode.nodeTypes.DOC) {
+            const d3txt = plot.selectAll("foreignObject")
+                .data([this])
+                .enter()
+                .append("foreignObject")
+                .attr("x", this.x - GraphNode.consts.RECT_WIDTH/2)
+                .attr("y", this.y + GraphNode.consts.RECT_HEIGHT/4)
+                .attr("height", 2 * GraphNode.consts.RECT_HEIGHT)
+                .attr("width", 2 * GraphNode.consts.RECT_WIDTH)
+                .append("xhtml:div")
+                .attr("id", "editable-p")
+                .attr("contentEditable", "true")
+                .style("text-align", "center")
+                .text(this.label)
+                .on("mousedown", (event, d) => {
+                    event.stopPropagation();
+                });
+            d3txt.node().focus();
+        }
     }
 }
